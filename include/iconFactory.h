f5a4f8d114db6a405239f301e0b060b62d78593c
hsu0694 <113703003@g.nccu.edu.tw>
2025-06-10 16:06:06 +0800

    Create iconFactory.h

---
+++ include/iconFactory.h
@@ -0,0 +1,19 @@
+// TODO 
+// Finish the declaration of IconFactory that
+// 1. offers some methods to create an Icon.
+// 2. encapsulates the constructor from the caller.
+
+#ifndef ICON_FACTORY_H
+#define ICON_FACTORY_H
+
+#include "icon.h"
+
+namespace IconFactory {
+    Icon blueBlock();
+    Icon redPlayer();
+	Icon Player();
+	Icon BOMBPlayer();
+	Icon APPLEPlayer();	
+}
+
+#endif
